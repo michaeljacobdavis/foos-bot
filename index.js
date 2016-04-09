63915e3b5823fa6d6b1547b764ccd1db1192c5eb
Mike Davis <mike@michaeljacobdavis.com>
2016-04-08 23:18:32 -0700

    Add ranking and win reporting

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Bot = require('slacky');
 const WebClient = require('@slack/client').WebClient;
-const startListen = require('./lib/start');
-const inListen = require('./lib/in');
-const stopListen = require('./lib/stop');
-const outListen = require('./lib/out');
+const startListen = require('./lib/listeners/start');
+const inListen = require('./lib/listeners/in');
+const stopListen = require('./lib/listeners/stop');
+const outListen = require('./lib/listeners/out');
+const winListen = require('./lib/listeners/win');
 const token = process.env.SLACK_API_TOKEN || '';
 
 module.exports = bot = new Bot(token);
@@ -13,7 +14,7 @@ bot.currentGames = {};
 bot.maximum = 4;
 
 // Add listeners
-[startListen, inListen, outListen, stopListen].forEach((listener) => {
+[startListen, inListen, outListen, stopListen, winListen].forEach((listener) => {
   bot.listen(listener.matcher, listener.callback);
 })
 
