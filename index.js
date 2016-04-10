ab36100658dd0f8a401b08f88c436ea34daa4264
Mike Davis <mike@michaeljacobdavis.com>
2016-04-10 11:14:37 -0700

    Added help

--- index.js
+++ index.js
@@ -6,17 +6,18 @@ const inListen = require('./lib/listeners/in');
 const stopListen = require('./lib/listeners/stop');
 const outListen = require('./lib/listeners/out');
 const winListen = require('./lib/listeners/win');
+const helpListen = require('./lib/listeners/help');
 const token = process.env.SLACK_API_TOKEN || '';
 
 module.exports = bot = new Bot(token);
 
-bot.use(whitelist(['foos']));
+bot.use(whitelist(['foos', 'mikestest']));
 bot.web = new WebClient(token);
 bot.currentGames = {};
 bot.maximum = 4;
 
 // Add listeners
-[startListen, inListen, outListen, stopListen, winListen].forEach((listener) => {
+[startListen, inListen, outListen, stopListen, winListen, helpListen].forEach((listener) => {
   bot.listen(listener.matcher, listener.callback);
 })
 
